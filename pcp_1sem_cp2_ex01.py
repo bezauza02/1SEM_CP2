9f881029ed6f7db089d82993dbf24ba90d72f2fc
Bruno <149333989+Brunoo7@users.noreply.github.com>
2026-04-25 17:23:47 -0300

    Exercício 1

--- pcp_1sem_cp2_ex01.py
+++ pcp_1sem_cp2_ex01.py
@@ -1,36 +1,34 @@
+#Variáveis
+cod_estado = int(input("Digite o código do estado de origem da carga do caminhão (1 a 5): "))
+peso = float(input("Digite o peso da carga do caminhão (Em Toneladas): "))
+cod_carga = int(input("Digite o código da carga (10 a 40): "))
 
-estado = int(input("Código do estado (1 a 5): "))
-peso_ton = float(input("Peso da carga (toneladas): "))
-codigo_carga = int(input("Código da carga (10 a 40): "))
+#Peso da carga do caminhão convertida em Kg e preço da carga do caminhão
 
+peso_kg = peso * 1000
 
-peso_kg = peso_ton * 1000
+if cod_carga >= 10 and cod_carga <= 20:
+    preco_carga = peso_kg  * 100
+elif cod_carga >= 21 and cod_carga <= 30:
+    preco_carga = peso_kg * 250
+elif cod_carga >= 31 and cod_carga <= 40:
+    preco_carga = peso_kg * 340
 
+#Valor do imposto
+if cod_estado == 1:
+    valor_impst = preco_carga * 0.35
+elif cod_estado == 2:
+    valor_impst = preco_carga * 0.25
+elif cod_estado == 3:
+    valor_impst = preco_carga * 0.15
+elif cod_estado == 4:
+    valor_impst = preco_carga * 0.05
+elif cod_estado == 5:
+    valor_impst = 0
 
-if 10 <= codigo_carga <= 20:
-    preco_kg = 100
-elif 21 <= codigo_carga <= 30:
-    preco_kg = 250
-else:
-    preco_kg = 340
-
-preco_total = peso_kg * preco_kg
-
-
-if estado == 1:
-    imposto = preco_total * 0.35
-elif estado == 2:
-    imposto = preco_total * 0.25
-elif estado == 3:
-    imposto = preco_total * 0.15
-elif estado == 4:
-    imposto = preco_total * 0.05
-else:
-    imposto = 0
-
-total = preco_total + imposto
-
-print(f"Peso em kg: {peso_kg}")
-print(f"Preço da carga: R$ {preco_total:.2f}")
-print(f"Imposto: R$ {imposto:.2f}")
-print(f"Valor total: R$ {total:.2f}")
+#Valor total
+vlr_total = valor_impst + preco_carga
+print(peso_kg)
+print(preco_carga)
+print(valor_impst)
+print(vlr_total)
